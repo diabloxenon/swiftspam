1a366f6cf14bca5fd87027ff39a78396c178b689
Naman Bishnoi <researchlabs@ncrypto.co>
2020-05-13 01:34:33 +0530

    🍿 Core commits 1/3. tweaking files, clearing stuff and organising things
    Signed-off-by: Naman Bishnoi <researchlabs@ncrypto.co>

---
+++ Swiftspam.playgroundbook/Contents/Chapters/Swiftspam.playgroundchapter/Pages/Canvas.playgroundpage/main.swift
@@ -0,0 +1,19 @@
+//#-hidden-code
+//
+//  main.swift
+//  
+//  Copyright © 2016-2019 Apple Inc. All rights reserved.
+//
+//#-end-hidden-code
+//#-code-completion(module, hide, Swift)
+//#-code-completion(identifier, hide, AbstractDrawable, _ColorLiteralType, _setup())
+//#-hidden-code
+_setup()
+_start()
+
+//#-end-hidden-code
+//#-editable-code Tap to enter code
+
+// Nothing to See here.
+
+//#-end-editable-code
